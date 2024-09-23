584ab0b152b17ccf0c08a081a226613c3af6d044
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-09-23 21:38:04 +0330

    initial_selection

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-if __name__ == '__main__':
+import random
 
+
+def initial_selection(my_list):
+    xi = []
+    for ml in my_list:
+        curr_c = random.choice([0, 1])
+        xi.append(curr_c)
+    return xi
+
+if __name__ == '__main__':
     capacity = 100
     wi = [50, 30, 40, 60, 80, 90, 10, 20]
     pi = [10, 3, 12, 40, 5, 15, 4, 22]

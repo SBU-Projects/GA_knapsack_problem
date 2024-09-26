c13e0e863da3b7c89abb0e115b55ff51105b4da0
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-09-26 18:13:28 +0330

    GA init

--- functions.py
+++ functions.py
@@ -3,3 +3,6 @@ class GeneticAlgorithm:
     def __init__(self, dataframe):
         print("Genetic Algorithm initialised!")
         self.dataframe = dataframe
+        self.MUTATION_RATE = 0.02
+        self.CROSSOVER_RATE = 0.2
+        self.REPRODUCTION_RATE = 0.78
